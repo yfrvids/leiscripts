f7e90fbd42a685e984be2a990f6fcefa9e79b4d8
debUser <ormaza.main@gmail.com>
2026-02-16 14:50:34 -0800

    chore: Add system volume variable

--- scripts/dotrec/dotrecscripts/dotreccontrolesavanzados.sh
+++ scripts/dotrec/dotrecscripts/dotreccontrolesavanzados.sh
@@ -3,13 +3,15 @@
 audiosistema=alsa_output.pci-0000_00_1b.0.analog-stereo.monitor
 audiomic=alsa_input.pci-0000_00_1b.0.analog-stereo
 
+volsystem=-10
+
 ffmpeg \
  -f pulse -i "$audiomic" \
  -f pulse -i "$audiosistema" \
  -f x11grab -video_size 1024x768 -framerate 30 -i :0.0+0,0 \
  -filter_complex \
    "[0:a]adelay=0|0,volume=5dB[mic]; \
-    [1:a]adelay=150|150,volume=-10dB[sistema]; \
+    [1:a]adelay=150|150,volume="$volsystem"dB[sistema]; \
     [mic][sistema]amix=inputs=2:duration=longest[audio_final]" \
  -map 2:v -map "[audio_final]" \
  -c:v libx264 -preset veryfast \
