5642360d444bfbae9459e960824f0a0177469cc0
Yfr <97921335+yfrvids@users.noreply.github.com>
2026-05-11 20:39:01 -0500

    Update dotrecontrolesavanzados.bat

--- scripts/dotrec/dotrecscripts/dotrecontrolesavanzados.bat
+++ scripts/dotrec/dotrecscripts/dotrecontrolesavanzados.bat
@@ -1,9 +1,18 @@
 @echo off
 cls
 
+for /f "tokens=1,* delims==" %%A in (%USERPROFILE%\.sourcesdotrec.txt) do (
+    set %%A=%%B
+)
+
 echo ====================================
 echo        FFMPEG WINDOWS REC
 echo ====================================
+echo.
+
+echo Audio Sistema: %audioSistemaWin%
+echo Audio Mic: %audioMicWin%
+echo Display: %displayWin%
 echo.
 
 set /p videoSize="Resolucion (1366x768): "
@@ -24,16 +33,16 @@ if "%nombregrabacion%"=="" set nombregrabacion=output
 
 ffmpeg ^
 -f dshow ^
--i audio="Microphone (Realtek(R) Audio)" ^
+-i audio="%audioMicWin%" ^
 -f dshow ^
--i audio="CABLE Output (VB-Audio Virtual Cable)" ^
+-i audio="%audioSistemaWin%" ^
 -f gdigrab ^
 -framerate %fps% ^
 -offset_x %offsetX% ^
 -offset_y %offsetY% ^
 -video_size %videoSize% ^
--i desktop ^
--filter_complex "[0:a]volume=%volmic%dB[mic];[1:a]volume=%volsystem%dB[sistema];[mic][sistema]amix=inputs=2:duration=longest[audio_final]" ^
+-i %displayWin% ^
+-filter_complex "[0:a]volume=%volmic%dB[mic];[1:a]volume=%volsystem%dB[sistema];[mic][sistema]amix=inputs=2:duration=longest:normalize=0[audio_final]" ^
 -map 2:v ^
 -map "[audio_final]" ^
 -c:v libx264 ^
